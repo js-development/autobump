f5c1755a393c6e391c3a2bc36b435a99358f93f1
js-development <60118490+js-development@users.noreply.github.com>
2020-01-24 01:51:15 -0500

    Update autobump.js

--- autobump.js
+++ autobump.js
@@ -1,7 +1,5 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const ms = require("ms");
-
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -11,34 +9,7 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-));
-
-module.exports.run = async(client, message, args) => {
-
-  const interval = setInterval( () => {
-
-     setTimeout(function(){
-    message.channel.send(`!d bump`);
-    }, ms(500));
-
-    #setTimeout(function(){
-    #message.channel.send(`dc!bump`);
-    #}, ms(21600001));
-  }, 10000);
-
-  #const collector = message.channel.createMessageCollector(() => true);
-  #collector.on("collect", m => {
-  #  if(m.content === "stop") {
-  #    clearInterval(interval);
-  #    collector.stop();
-  #  }
-  #})
-}
-
-
-module.exports.help = {
-  name: "autobump"
-}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
